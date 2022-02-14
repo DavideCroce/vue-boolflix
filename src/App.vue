feb9f85f92e252516b0d2dc74a4440ca5b807ef8
IskahnX <davidecroce10@gmail.com>
2022-02-14 15:32:19 +0100

    add axios and the movies api

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-<div class="container">ciao</div>
+<div class="container">
+  <div class="search-bar">
+    <input type="text" placeholder="Cerca un titolo..." :for="movie in searchMovies()" :key="movie.title">
+    <button>Cerca</button>
+  </div>
+</div>
 </template>
 
 <script>
@@ -8,7 +13,20 @@ export default {
   name: 'App',
   components: {
     
-  }
+  },
+  data(){
+    return {
+      movies: [],
+    }
+  },
+  methods: {
+    searchMovies(){
+      axios.get("https://api.themoviedb.org/3/search/movie?api_key=c2b4899b88804168313526c583bf26b2&query=games&language=it-IT").then((res) => {
+      this.movies = res.results.response;
+      this.movie.push(movies);
+      });
+    },
+  },
 }
 </script>
 
