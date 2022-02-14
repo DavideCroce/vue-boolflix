523f6f838b3fdf4c9f8e8e762b9da9daea6f09f3
IskahnX <davidecroce10@gmail.com>
2022-02-14 15:41:59 +0100

    added cicle for on the movie array

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
 <div class="container">
   <div class="search-bar">
-    <input type="text" placeholder="Cerca un titolo..." :for="movie in searchMovies()" :key="movie.title">
-    <button>Cerca</button>
+    <input type="text" placeholder="Cerca un titolo..." >
+    <button @click="searchMovies()">Cerca</button>
+  </div>
+  <div class="movies-list">
+    <ul :for="movie in movies" :key="movie.title">
+      <li>
+        {{movie.title}}
+        {{movie.original_title}}
+        {{movie.original_language}}
+        {{movie.vote_average}}
+      </li>
+    </ul>
   </div>
 </div>
 </template>
@@ -23,7 +33,7 @@ export default {
     searchMovies(){
       axios.get("https://api.themoviedb.org/3/search/movie?api_key=c2b4899b88804168313526c583bf26b2&query=games&language=it-IT").then((res) => {
       this.movies = res.results.response;
-      this.movie.push(movies);
+      this.push(movies);
       });
     },
   },
